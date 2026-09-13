33b31e924a0f2e1c2033f53fc1c7e14b0b84f243
kellermann-am <163904019+kellermann-am@users.noreply.github.com>
2026-09-13 03:04:28 +0300

    Update build-mobile-xcframework.sh

--- OlcRTC-iOS/Scripts/build-mobile-xcframework.sh
+++ OlcRTC-iOS/Scripts/build-mobile-xcframework.sh
@@ -5,7 +5,7 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 VENDOR_DIR="$ROOT_DIR/Vendor"
 FRAMEWORK_DIR="$ROOT_DIR/Frameworks"
 OLCRTC_DIR="$VENDOR_DIR/olcrtc"
-OLCRTC_REF="${OLCRTC_REF:-refactor/universal-carrier}"
+OLCRTC_REF="${OLCRTC_REF:-master}"
 
 mkdir -p "$VENDOR_DIR" "$FRAMEWORK_DIR"
 
